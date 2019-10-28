0fb70e07c5ba9caea57953a40163769dbef424ee
myMiDad <1277420858@qq.com>
2019-10-29 00:14:15 +0800

    first commit solr

---
+++ solr-01/src/main/java/com/tom/solr/Solr01Application.java
@@ -0,0 +1,13 @@
+package com.tom.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Solr01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Solr01Application.class, args);
+    }
+
+}
---
+++ solr-01/src/test/java/com/tom/solr/Solr01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.solr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Solr01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
